71eac6254134bc03341b063153eabc5969a9a482
Sxj1997 <1090306920@qq.com>
2021-10-26 18:06:52 +0800

    1026_2

---
+++ src2/src/dotcom.java
@@ -0,0 +1,14 @@
+public class dotcom {
+    private String []zuobiao=new String[3];
+    private int life=3;
+
+    public String[] getZuobiao() {
+        return zuobiao;
+    }
+    public void setZuobiao(String zuobiao[]){
+        zuobiao=this.zuobiao;
+    }
+    public void shut(String a){
+        if
+    }
+}
